88699feaa60c153f64f3797db7965b7434a7c99e
Vikous44 <108953581+Vikous44@users.noreply.github.com>
2022-07-09 21:39:33 +0200

    collision

--- main.ts
+++ main.ts
@@ -5,24 +5,47 @@ function playercontroller () {
         player1left()
     }
 }
+function checkcollision () {
+    if (aMeteorsY[playerX] == 4) {
+        pause2 = 1
+        hp += -1
+        basic.showIcon(IconNames.Sad)
+        basic.pause(2000)
+        pause2 = 0
+        init()
+    }
+}
 function player1right () {
     basic.pause(70)
-    player1 += 1
-    if (player1 == 5) {
-        player1 = 4
+    playerX += 1
+    if (playerX == 5) {
+        playerX = 4
     }
-    led.unplot(player1 - 1, 4)
+    led.unplot(playerX - 1, 4)
+}
+function init () {
+    basic.clearScreen()
+    aMeteorsY = [
+    0,
+    0,
+    0,
+    0,
+    0
+    ]
+    for (let index = 0; index <= 4; index++) {
+        aMeteorsY[index] = randint(-15, -1)
+    }
 }
 function player1left () {
     basic.pause(70)
-    player1 += -1
-    if (player1 == -1) {
-        player1 = 0
+    playerX += -1
+    if (playerX == -1) {
+        playerX = 0
     }
-    led.unplot(player1 + 1, 4)
+    led.unplot(playerX + 1, 4)
 }
 function render () {
-    led.plot(player1, 4)
+    led.plot(playerX, 4)
     for (let x = 0; x <= 4; x++) {
         led.plot(x, aMeteorsY[x])
     }
@@ -40,25 +63,26 @@ function fallmeteor () {
 }
 let y = 0
 let aMeteorsY: number[] = []
-let player1 = 0
-player1 = 2
-aMeteorsY = [
-0,
-0,
-0,
-0,
-0
-]
-for (let index = 0; index <= 4; index++) {
-    aMeteorsY[index] = randint(-15, -1)
-}
+let playerX = 0
+let pause2 = 0
+pause2 = 0
+let hp = 5
+playerX = 2
+init()
 basic.forever(function () {
-    playercontroller()
+    if (pause2 == 0) {
+        fallmeteor()
+        basic.pause(170)
+    }
 })
 basic.forever(function () {
-    fallmeteor()
-    basic.pause(170)
+    if (pause2 == 0) {
+        render()
+        checkcollision()
+    }
 })
 basic.forever(function () {
-    render()
+    if (pause2 == 0) {
+        playercontroller()
+    }
 })
