0adc87681be8e287f7e0487aec9086b5773822d0
Vikous44 <108953581+Vikous44@users.noreply.github.com>
2022-07-09 20:09:42 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,30 +8,26 @@ function playercontroller () {
 function player1right () {
     basic.pause(70)
     player1 += 1
-    if (player1 == -1) {
-        player1 = 0
+    if (player1 == 5) {
+        player1 = 4
     }
     led.unplot(player1 - 1, 4)
 }
 function player1left () {
     basic.pause(70)
     player1 += -1
-    if (player1 == 5) {
-        player1 = 4
+    if (player1 == -1) {
+        player1 = 0
     }
     led.unplot(player1 + 1, 4)
 }
 function render () {
     led.plot(player1, 4)
 }
-let list: number[] = []
 let player1 = 0
 led.plot(2, 4)
 player1 = 3
 basic.forever(function () {
     playercontroller()
     render()
-})
-basic.forever(function () {
-    list = [0, 1]
 })
